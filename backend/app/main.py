8aad50bcd14516f5a7e2e0db4262ead70726b1e6
hcheong <hao.cheong@outlook.com>
2023-11-11 17:21:33 +1100

    Added routers to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,19 @@
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-
-import cruds
-import models
-import schemas
-import endpoints
-import metadata
-from database import SessionLocal, engine
 from fastapi.middleware.cors import CORSMiddleware
 
-# models.Base.metadata.drop_all(bind=engine)
-models.Base.metadata.create_all(bind=engine)
+import app.metadata as metadata
+import app.database as database
+
+from app.database import engine
+
+import app.endpoints.operation_endpoints as operation_endpoints
+import app.endpoints.bucket_endpoints as bucket_endpoints
+import app.endpoints.flow_event_endpoints as flow_event_endpoints
+import app.endpoints.log_endpoints as log_endpoints
+
+database.Base.metadata.create_all(bind=engine)
 
 # Initialising instance of the backend
 app = FastAPI(title="Spenny", openapi_tags=metadata.tags_metadata)
@@ -27,23 +28,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Gets an instance of the DB, will close connection with DB when done
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 # ======== ROOT ENDPOINT ========
 # Not necessary but good indication that connection been made
-
 
 @app.get("/")
 def root():
     return {"connection": True}
 
+app.include_router(operation_endpoints.router)
+app.include_router(bucket_endpoints.routers)
+app.include_router(flow_event_endpoints.routers)
+app.include_router(log_endpoints.routers)
 
-app.include_router(endpoints.router)
