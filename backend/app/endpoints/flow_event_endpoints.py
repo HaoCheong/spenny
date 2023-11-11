ceb888a10796a1ee01c53af38470283ebfd6667a
hcheong <hao.cheong@outlook.com>
2023-11-11 17:20:41 +1100

    Split endpoints

---
+++ backend/app/endpoints/flow_event_endpoints.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from helpers import get_db
+from fastapi import Depends, FastAPI, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+
+import app.schemas.flow_event_schemas as schemas
+import app.cruds.flow_event_cruds as cruds
+
+router = APIRouter()
+
+# ======== FLOW EVENT ENDPOINT ========
+
+
+@router.post("/flowEvent", response_model=schemas.FlowEventReadNR, tags=['flowEvent'])
+def create_flowEvent(flowEvent: schemas.FlowEventCreate, db: Session = Depends(get_db)):
+    return cruds.create_flowEvent(db=db, flowEvent=flowEvent)
+
+
+@router.get("/flowEvents", response_model=List[schemas.FlowEventReadNR], tags=['flowEvent'])
+def get_all_flowEvents(limit: int = 50, db: Session = Depends(get_db)):
+    return cruds.get_all_flowEvents(db=db, limit=limit)
+
+
+@router.get('/flowEvent/{flowEvent_id}', response_model=schemas.FlowEventReadWR, tags=['flowEvent'])
+def get_flowEvent_by_id(flowEvent_id: int, db: Session = Depends(get_db)):
+    db_flowEvent = cruds.get_flowEvent_by_id(db=db, id=flowEvent_id)
+    if not db_flowEvent:
+        raise HTTPException(status_code=400, detail="FlowEvent does not exist")
+
+    return db_flowEvent
+
+
+@router.patch('/flowEvent/{flowEvent_id}', response_model=schemas.FlowEventReadWR, tags=['flowEvent'])
+def update_flowEvent_by_id(flowEvent_id: int, new_flowEvent: schemas.FlowEventUpdate, db: Session = Depends(get_db)):
+    db_flowEvent = cruds.get_flowEvent_by_id(db=db, id=flowEvent_id)
+    if not db_flowEvent:
+        raise HTTPException(status_code=400, detail="FlowEvent does not exist")
+
+    return cruds.update_flowEvent_by_id(db=db, id=flowEvent_id, new_flowEvent=new_flowEvent)
+
+
+@router.delete('/flowEvent/{flowEvent_id}', tags=['flowEvent'])
+def delete_flowEvent_by_id(flowEvent_id: int, db: Session = Depends(get_db)):
+    db_flowEvent = cruds.get_flowEvent_by_id(db, id=flowEvent_id)
+    if not db_flowEvent:
+        raise HTTPException(status_code=400, detail="FlowEvent does not exist")
+
+    return cruds.delete_flowEvent_by_id(db, id=flowEvent_id)
