db21b50db5fb285dd77bf9662bf5095f93d24766
hcheong <hao.cheong@outlook.com>
2023-12-14 11:40:35 +1100

    Removed old files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css';
-import Dashboard from './Routes/dashboard';
-import DashboardBasic from './Routes/dashboardBasic';
+import Dashboard from './Routes/Dashboard';
 import { ChakraProvider } from '@chakra-ui/react'
 const App = () => {
   return (
@@ -10,8 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/DashboardBasic" element={<DashboardBasic />} />
-          <Route path="*" element={<Navigate to="DashboardBasic" replace />} />
+          <Route path="*" element={<Navigate to="/Dashboard" replace />} />
         </Routes>
 
       </BrowserRouter>
