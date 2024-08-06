932517863770fd617219b78f49da54f5aef275db
hao.cheong <61342120+HaoCheong@users.noreply.github.com>
2024-08-06 14:28:59 +1000

    Adjusted APIs to use api/v1 styling

--- backend/app/endpoints/log_endpoints.py
+++ backend/app/endpoints/log_endpoints.py
@@ -13,31 +13,31 @@ router = APIRouter()
 # ======== LOG ENDPOINT ========
 
 
-@router.post("/log", response_model=schemas.LogReadNR, tags=['Log'])
+@router.post("/api/v1/log", response_model=schemas.LogReadNR, tags=['Log'])
 def create_log(log: schemas.LogCreate, db: Session = Depends(get_db)):
     return cruds.create_log(db=db, log=log)
 
 
-@router.get("/logs", response_model=List[schemas.LogReadNR], tags=['Log'])
+@router.get("/api/v1/logs", response_model=List[schemas.LogReadNR], tags=['Log'])
 def get_all_logs(limit: int = 1000, db: Session = Depends(get_db)):
     return cruds.get_all_logs(db=db, limit=limit)
 
 
-@router.get("/logs/{bucket_id}/{date}", response_model=List[schemas.LogReadNR], tags=['Log'])
+@router.get("/api/v1/logs/{bucket_id}/{date}", response_model=List[schemas.LogReadNR], tags=['Log'])
 def get_all_logs_by_bucket_id(bucket_id: int, date: str, db: Session = Depends(get_db)):
     return cruds.get_all_logs_by_bucket_id(db=db, date=date, bucket_id=bucket_id)
 
 
-@router.get('/log/{log_id}', response_model=schemas.LogReadWR, tags=['Log'])
+@router.get('/api/v1/log/{log_id}', response_model=schemas.LogReadWR, tags=['Log'])
 def get_log_by_id(log_id: int, db: Session = Depends(get_db)):
     db_log = cruds.get_log_by_id(db=db, id=log_id)
     if not db_log:
         raise HTTPException(status_code=400, detail="Log does not exist")
-    
+
     return db_log
 
 
-@router.patch('/log/{log_id}', response_model=schemas.LogReadWR, tags=['Log'])
+@router.patch('/api/v1/log/{log_id}', response_model=schemas.LogReadWR, tags=['Log'])
 def update_log_by_id(log_id: int, new_log: schemas.LogUpdate, db: Session = Depends(get_db)):
     db_log = cruds.get_log_by_id(db=db, id=log_id)
     if not db_log:
@@ -46,7 +46,7 @@ def update_log_by_id(log_id: int, new_log: schemas.LogUpdate, db: Session = Depe
     return cruds.update_log_by_id(db=db, id=log_id, new_log=new_log)
 
 
-@router.delete('/log/{log_id}', tags=['Log'])
+@router.delete('/api/v1/log/{log_id}', tags=['Log'])
 def delete_log_by_id(log_id: int, db: Session = Depends(get_db)):
     db_log = cruds.get_log_by_id(db, id=log_id)
     if not db_log:
--- backend/app/endpoints/operation_endpoints.py
+++ backend/app/endpoints/operation_endpoints.py
@@ -14,26 +14,33 @@ router = APIRouter()
 # ========== OPERATION =========
 
 # When trigger it will run this application and update all the flow amount to the current date
-@router.put('/updateValues', tags=['Operations'])
+
+
+@router.put('/api/v1/updateValues', tags=['Operations'])
 def update_all_buckets(db: Session = Depends(get_db)):
     return bucket_operations.update_all_buckets(db=db)
 
 
-@router.put('/soloTrigger', tags=['Operations'])
+@router.put('/api/v1/soloTrigger', tags=['Operations'])
 def solo_trigger(trigger: schemas.TriggerBase, db: Session = Depends(get_db)):
     return trigger_operations.solo_trigger(trigger=trigger, db=db)
 
-@router.put('/bringForward', tags=['Operations'])
+
+@router.put('/api/v1/bringForward', tags=['Operations'])
 def bring_forward(details: schemas.BringForwardBase, db: Session = Depends(get_db)):
 
-    db_flowEvent = flow_event_cruds.get_flowEvent_by_id(db, id=details.flow_event_id)
+    db_flowEvent = flow_event_cruds.get_flowEvent_by_id(
+        db, id=details.flow_event_id)
     if not db_flowEvent:
-        raise HTTPException(status_code=400, detail="Flow Event does not exist")
-    
+        raise HTTPException(
+            status_code=400, detail="Flow Event does not exist")
+
     return trigger_operations.bringForward(details=details, db=db)
 
 # ========== TEST ==========
 # A test endpoint to easily test our certain functions via swagger
+
+
 @router.get('/TEST')
 def test(db: Session = Depends(get_db)):
     return bucket_cruds.get_bucket_flowEvents(bucket_id=1, db=db)
