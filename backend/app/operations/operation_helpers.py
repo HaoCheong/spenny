69abdc8370360cfe12510c2f43d612d2908cacaa
hao.cheong <61342120+HaoCheong@users.noreply.github.com>
2024-08-21 14:58:11 +1000

    Added MULT function

--- backend/app/operations/operation_helpers.py
+++ backend/app/operations/operation_helpers.py
@@ -21,28 +21,13 @@ def change_bucket_value(bucket: BucketReadWR, op: str, change_amount: float, db:
         new_value = round(
             bucket["current_amount"] + change_amount, 2)
 
+    elif (op == "MULT"):
+        new_value = round(
+            bucket["current_amount"] * (1 + change_amount), 2)
+
     new_bucket = BucketUpdate(current_amount=new_value)
 
     update_bucket_by_id(
         db=db, id=bucket["id"], new_bucket=new_bucket)
 
     return new_bucket
-
-
-def log_operation(log_details: Dict, log_time: datetime, db: Session) -> LogReadNR:
-    '''
-    Wraps the creation and the logging
-    '''
-
-    # PFIX: IDK if this really simplifies anything
-    new_log = LogCreate(
-        name=log_details["name"],
-        description=log_details["description"],
-        type=log_details['type'],
-        amount=(log_details["change_amount"]) * -1,
-        date_created=log_time,
-        bucket_id=log_details["from_bucket_id"]
-    )
-    create_log(db=db, log=new_log)
-
-    return new_log
