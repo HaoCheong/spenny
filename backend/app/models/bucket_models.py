4db1327f322d4ce660527c546409b284b131e545
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-05-01 11:24:11 +1000

    Updated requirements and updated models

--- backend/app/models/bucket_models.py
+++ backend/app/models/bucket_models.py
@@ -1,16 +1,17 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Integer, String, Boolean, DateTime
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 from app.database.database import Base
+from datetime import datetime
 
 
 class Bucket(Base):
 
     __tablename__ = "buckets"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
-    amount = Column(Integer)
-    is_invisible = Column(Boolean)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    amount: Mapped[int] = mapped_column(Integer)
+    is_invisible: Mapped[bool] = mapped_column(Boolean)
+    created_at: Mapped[datetime] = mapped_column(DateTime)
+    updated_at: Mapped[datetime] = mapped_column(DateTime)
--- backend/app/models/event_models.py
+++ backend/app/models/event_models.py
@@ -1,18 +1,19 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, JSON
-from sqlalchemy.orm import relationship
+from sqlalchemy import Integer, String, Boolean, DateTime, JSON
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 from app.database.database import Base
+from datetime import datetime
 
 
 class Event(Base):
 
     __tablename__ = "events"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
-    trigger_datetime = Column(DateTime)
-    frequency = Column(String)
-    event_type = Column(String)
-    properties = Column(JSON)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    trigger_datetime: Mapped[datetime] = mapped_column(DateTime)
+    frequency: Mapped[str] = mapped_column(String)
+    event_type: Mapped[str] = mapped_column(String)
+    properties: Mapped[dict] = mapped_column(JSON)
+    created_at: Mapped[datetime] = mapped_column(DateTime)
+    updated_at: Mapped[datetime] = mapped_column(DateTime)
