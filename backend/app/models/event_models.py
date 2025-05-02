3d8a4f5e2b8c6cad97b2ab861bde0b9d6acc54d4
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-05-02 12:14:24 +1000

    Added events to bucket as a m:1 relationship, need fixing type check

--- backend/app/models/event_models.py
+++ backend/app/models/event_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Boolean, DateTime, JSON
+from sqlalchemy import Integer, String, Boolean, DateTime, JSON, ForeignKey
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 from app.database.database import Base
 from datetime import datetime
@@ -17,3 +17,8 @@ class Event(Base):
     properties: Mapped[dict] = mapped_column(JSON)
     created_at: Mapped[datetime] = mapped_column(DateTime)
     updated_at: Mapped[datetime] = mapped_column(DateTime)
+
+    bucket_id: Mapped[int | None] = mapped_column(
+        Integer, ForeignKey("buckets.id"), nullable=True)
+    bucket: Mapped["Bucket"] = relationship(
+        "Bucket", back_populates="events", uselist=False)
