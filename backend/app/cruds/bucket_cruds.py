bdc8d17d6d4caae05a5cbd59e3ec360f8b7912e9
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-05-08 14:34:21 +1000

    Allowed none for updating event type, tested calling from the model, alter event and bucket updated_at permissions via the schema

--- backend/app/cruds/bucket_cruds.py
+++ backend/app/cruds/bucket_cruds.py
@@ -38,7 +38,8 @@ def get_all_buckets(db: Session, skip: int = 0, limit: int = 100):
 
 def get_bucket_by_id(db: Session, id: str):
     ''' Get specific instance of bucket based on provided bucket ID '''
-    return db.query(model.Bucket).filter(model.Bucket.id == id).first()
+    db_bucket = db.query(model.Bucket).filter(model.Bucket.id == id).first()
+    return db_bucket
 
 
 def update_bucket_by_id(db: Session, id: int, new_bucket: schemas.BucketUpdate):
