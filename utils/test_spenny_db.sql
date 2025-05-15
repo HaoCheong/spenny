21bdbb9d74afd304e7252ffdf4c290d8885d2018
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-05-15 16:34:17 +1000

    Added tests files stubs, updated the spenny DB dump to include log type

--- utils/test_spenny_db.sql
+++ utils/test_spenny_db.sql
@@ -3,7 +3,7 @@
 --
 
 -- Dumped from database version 14.5 (Debian 14.5-2.pgdg110+2)
--- Dumped by pg_dump version 14.17 (Ubuntu 14.17-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 16.8 (Ubuntu 16.8-0ubuntu0.24.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -15,6 +15,13 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+-- *not* creating schema, since initdb creates it
+
 
 SET default_tablespace = '';
 
@@ -108,7 +115,8 @@ CREATE TABLE public.logs (
     bucket_id integer NOT NULL,
     bucket_name character varying NOT NULL,
     created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    updated_at timestamp without time zone NOT NULL,
+    log_type character varying
 );
 
 
@@ -199,7 +207,7 @@ COPY public.events (id, name, description, trigger_datetime, frequency, event_ty
 -- Data for Name: logs; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.logs (id, name, description, event_id, event_type, event_properties, bucket_id, bucket_name, created_at, updated_at) FROM stdin;
+COPY public.logs (id, name, description, event_id, event_type, event_properties, bucket_id, bucket_name, created_at, updated_at, log_type) FROM stdin;
 \.
 
 
@@ -275,6 +283,14 @@ CREATE INDEX ix_logs_id ON public.logs USING btree (id);
 
 ALTER TABLE ONLY public.events
     ADD CONSTRAINT events_bucket_id_fkey FOREIGN KEY (bucket_id) REFERENCES public.buckets(id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: -
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
