98f3c306497b19e9922862fe871ecaa69852d0a9
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-05-21 16:57:36 +1000

    Attached logging to the system, added the updater to run on specific api calls that are dependent

--- backend/app/endpoints/log_endpoints.py
+++ backend/app/endpoints/log_endpoints.py
@@ -6,23 +6,27 @@ from sqlalchemy.orm import Session
 
 import app.schemas.log_schemas as schemas
 import app.cruds.log_cruds as cruds
+import app.operations.event_operations as event_op
 
 router = APIRouter()
 
 
 @router.post("/api/v1/log", response_model=schemas.LogCreate, tags=["Logs"])
 def create_log(log: schemas.LogCreate, db: Session = Depends(get_db)):
+    event_op.EventOperation.update_all_events(db=db)
     return cruds.create_log(db=db, log=log)
 
 
 @router.get("/api/v1/logs", response_model=schemas.LogAllRead, tags=["Logs"])
 def get_all_logs(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    event_op.EventOperation.update_all_events(db=db)
     db_log_data = cruds.get_all_logs(db, skip, limit)
     return db_log_data
 
 
 @router.get("/api/v1/logs/{bucket_id}", response_model=schemas.LogAllRead, tags=["Logs"])
 def get_all_logs_by_bucket_id(bucket_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    event_op.EventOperation.update_all_events(db=db)
     db_log_data = cruds.get_all_logs_by_bucket_id(
         bucket_id=bucket_id, db=db, skip=skip, limit=limit)
     return db_log_data
@@ -30,6 +34,7 @@ def get_all_logs_by_bucket_id(bucket_id: int, skip: int = 0, limit: int = 100, d
 
 @router.post("/api/v1/logs/time_range", response_model=schemas.LogAllRead, tags=["Logs"])
 def get_all_logs_by_time_range(time_range: schemas.LogTimeRange, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    event_op.EventOperation.update_all_events(db=db)
     db_log_data = cruds.get_all_logs_by_time_range(
         start_date=time_range.start_date, end_date=time_range.end_data, db=db, skip=skip, limit=limit)
     return db_log_data
@@ -37,6 +42,7 @@ def get_all_logs_by_time_range(time_range: schemas.LogTimeRange, skip: int = 0, 
 
 @router.get("/api/v1/log/{log_id}", response_model=schemas.LogRead, tags=["Logs"])
 def get_log_by_id(log_id: int, db: Session = Depends(get_db)):
+    event_op.EventOperation.update_all_events(db=db)
     db_log = cruds.get_log_by_id(db, id=log_id)
     if not db_log:
         raise HTTPException(status_code=400, detail="Log does not exist")
@@ -46,6 +52,7 @@ def get_log_by_id(log_id: int, db: Session = Depends(get_db)):
 
 @router.delete("/api/v1/log/{log_id}", tags=["Logs"])
 def delete_log_by_id(log_id: int, db: Session = Depends(get_db)):
+    event_op.EventOperation.update_all_events(db=db)
     db_log = cruds.get_log_by_id(db, id=log_id)
     if not db_log:
         raise HTTPException(status_code=400, detail="Log does not exist")
