35236c74d27332e28a36fba125d5627936ae7182
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-06-20 13:30:12 +1000

    Added new pages, placeholder, and standard components

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Page from "../components/Page";
+import Section from "../components/Section";
+import Button from "../components/Button";
 
 const Dashboard = () => {
 	return (
 		<>
-			<h1 class="text-3xl font-bold underline">Dashboard</h1>
+			<Page>
+				<div
+					id="dashboard-content"
+					class="flex flex-col gap-3 h-full w-full"
+				>
+					<Section
+						classSize="h-1/8"
+						classStyle="flex flex-row items-center w-full gap-5"
+					>
+						<h1 class="w-5/8 text-5xl items-center">Dashboard</h1>
+						<Button classStyle="w-3/16" label="Add Bucket" />
+						<Button classStyle="w-3/16" label="Manual Trigger" />
+					</Section>
+					<Section classSize="h-7/8"></Section>
+				</div>
+			</Page>
 		</>
 	);
 };
