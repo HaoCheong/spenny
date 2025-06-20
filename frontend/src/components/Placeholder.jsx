5008b6cc517e76fd4ea568d30e7d97b58d1bd4bc
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-06-20 15:44:06 +1000

    Experimenting with sizes and custom log system

--- frontend/src/components/Placeholder.jsx
+++ frontend/src/components/Placeholder.jsx
@@ -1,7 +1,10 @@
-const Placeholder = ({ label = "PLACEHOLDER", classStyle }) => {
+const Placeholder = ({
+	label = "PLACEHOLDER",
+	classStyle = "w-full h-full",
+}) => {
 	return (
 		<div
-			className={`bg-green-600 rounded-xl w-full h-full flex justify-center items-center ${classStyle}`}
+			className={`border-5 border-solid border-white rounded-lg flex justify-center items-center ${classStyle}`}
 		>
 			{label}
 		</div>
