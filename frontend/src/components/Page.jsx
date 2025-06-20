617b80bc971bdfcc3d5f449c88a88b6caaf10e41
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-06-20 15:44:06 +1000

    Experimenting with sizes and custom log system

--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -2,13 +2,10 @@ import Sidebar from "./Sidebar";
 
 const Page = ({ children }) => {
 	return (
-		<div class="flex flex-row bg-teal-200 w-screen h-screen">
+		<div class="flex flex-roww-screen h-screen">
 			<Sidebar />
-			<div id="page-container" class="bg-black w-full h-full p-5">
-				<div
-					id="page-content"
-					class="w-full h-full border-2 border-solid"
-				>
+			<div id="page-container" class="bg-black w-4/5 h-full p-5">
+				<div id="page-content" class="h-full w-1500px">
 					{children}
 				</div>
 			</div>
