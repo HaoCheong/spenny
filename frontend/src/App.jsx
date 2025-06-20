14685e5090f3d4e6c0a74e44f5d271e7867d1f65
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-06-20 13:30:12 +1000

    Added new pages, placeholder, and standard components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 // import "./App.css";
 import Dashboard from "./routes/Dashboard";
+import Logs from "./routes/Logs";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/Dashboard" element={<Dashboard />} />
+				<Route path="/Logs" element={<Logs />} />
 				<Route
 					path="*"
 					element={<Navigate to="/Dashboard" replace />}
