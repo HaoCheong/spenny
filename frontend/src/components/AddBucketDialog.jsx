321fd2fcebe25ac05dbfd115a7d5c2a0f041ef58
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-06-27 17:06:56 +1000

    Finished base of dialog, and and changed accents

--- frontend/src/components/AddBucketDialog.jsx
+++ frontend/src/components/AddBucketDialog.jsx
@@ -1,55 +1,98 @@
 import {
+	Description,
 	Dialog,
-	DialogBackdrop,
 	DialogPanel,
 	DialogTitle,
+	Field,
+	Input,
+	Label,
+	Textarea,
 	Transition,
+	TransitionChild,
 } from "@headlessui/react";
 import Button from "./Button";
 import clsx from "clsx";
+import { Fragment } from "react";
+import DialogBase from "./DialogBase";
+import Textfield from "./Textfield";
 
 const AddBucketDialog = ({ isOpen, setIsOpen }) => {
 	const handleClose = () => {
 		setIsOpen(false);
 	};
 	return (
-		<Transition show={isOpen}>
-			<Dialog
-				open={isOpen}
-				as="div"
-				className="relative z-10 focus:outline-none"
-				onClose={handleClose}
+		<DialogBase isOpen={isOpen} setIsOpen={setIsOpen}>
+			<DialogPanel
+				transition
+				className={clsx(
+					"w-full max-w-2xl rounded-xl bg-spenny-background shadow-lg p-5",
+					"border-solid border-5 border-spenny-accent-primary",
+					"transition duration-200",
+					"data-closed:scale-90 data-closed:opacity-0",
+					"data-leave:duration-200 data-leave:ease-in-out"
+				)}
 			>
-				{/* `fixed inset-0 z-10 w-screen overflow-y-auto backdrop-blur-xl data-enter:backdrop-blur-xl data-enter:opacity-100 data-enter:duration-300 data-enter:ease-in data-leave:backdrop-blur-none data-leave:opacity-0 data-leave:duration-300 data-leave:ease-out` */}
-				<div
-					className={clsx(
-						"fixed inset-0 z-10 w-screen overflow-y-auto transition ease-in-out backdrop-blur-2xl",
-						"data-enter:bg-green-300 data-enter:opacity-90 data-enter:duration-500",
-						"data-transition:bg-blue-500 data-transition:opacity-20 data-transition:duration-500"
-					)}
+				<DialogTitle
+					as="h3"
+					className="text-3xl font-bold text-white pb-3"
 				>
-					<div className="flex min-h-full items-center justify-center p-4">
-						<DialogPanel
-							transition
-							className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-xl duration-100 ease-out data-closed:opacity-0"
-						>
-							<DialogTitle
-								as="h3"
-								className="text-base/7 font-medium text-white"
-							>
-								Test
-							</DialogTitle>
-							<p className="mt-2 text-sm/6 text-white/50">
-								Test Content
-							</p>
-							<div className="mt-4">
-								<Button onClick={handleClose} label="Close" />
-							</div>
-						</DialogPanel>
-					</div>
+					Add Bucket
+				</DialogTitle>
+				<div id="add-modal-input-content" class="flex flex-col gap-3">
+					<Field>
+						<Label className="text-md font-medium text-white">
+							Name
+						</Label>
+						<Input
+							className={clsx(
+								"mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+								"focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/30"
+							)}
+						/>
+					</Field>
+					<Field>
+						<Label className="text-md font-medium text-white">
+							Starting Amount
+						</Label>
+						<Input
+							className={clsx(
+								"mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+								"focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/30"
+							)}
+						/>
+					</Field>
+					<Field>
+						<Label className="text-md font-medium text-white">
+							Description
+						</Label>
+						<Description className="text-sm/6 text-white/50">
+							Short Description of what the bucket purpose is.
+						</Description>
+						<Textarea
+							className={clsx(
+								"mt-3 block w-full resize-none rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+								"focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25"
+							)}
+							rows={3}
+						/>
+					</Field>
 				</div>
-			</Dialog>
-		</Transition>
+				<div
+					id="dialog-action-panel"
+					className="flex flex-row-reverse w-full pt-3 gap-3"
+				>
+					<Button
+						classColor="border-solid border-2 border-solid bg-spenny-accent-warning text-black hover:bg-spenny-background hover:text-spenny-accent-warning"
+						label="Close Form"
+						onClick={handleClose}
+					/>
+					<Button
+						classColor="border-solid border-2 border-solid bg-spenny-accent-primary text-black hover:bg-spenny-background hover:text-spenny-accent-primary"
+						label="Add Bucket"
+					/>
+				</div>
+			</DialogPanel>
+		</DialogBase>
 	);
 };
 
