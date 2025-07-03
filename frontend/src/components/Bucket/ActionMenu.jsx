34ec535fbd8799c89ec9022d7eba415f442bcb96
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-07-03 14:56:16 +1000

    Updated the bucket card to include action menu, link up the view modal

--- frontend/src/components/Bucket/ActionMenu.jsx
+++ frontend/src/components/Bucket/ActionMenu.jsx
@@ -1,5 +1,35 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
 const ActionMenu = () => {
-	//TODO
+	return (
+		<Menu>
+			<MenuButton className="flex flex-col p-3 h-full justify-center items-center rounded-lg text-md transition duration-300 ease-in-out w-1/2 text-xl border-solid border-2 border-spenny-accent-warning bg-spenny-accent-warning text-black hover:bg-spenny-background hover:text-spenny-accent-warning">
+				Action
+			</MenuButton>
+
+			<MenuItems
+				transition
+				anchor="bottom end"
+				className="w-52 origin-top-right rounded-xl border border-white/5 bg-spenny-background p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0 flex flex-col gap-2"
+			>
+				<MenuItem>
+					<button className="flex w-full items-center gap-2 rounded-lg p-1.5 transition duration-300 ease-in-out bg-spenny-accent-primary/80 data-focus:bg-spenny-background data-focus:border-1 data-focus:border-solid data-focus:border-spenny-accent-primary/80 data-focus:text-spenny-accent-primary/80">
+						Manual Entry
+					</button>
+				</MenuItem>
+				<MenuItem>
+					<button className="flex w-full items-center gap-2 rounded-lg p-1.5 transition duration-300 ease-in-out bg-spenny-accent-warning/80 data-focus:bg-spenny-background data-focus:border-1 data-focus:border-solid data-focus:border-spenny-accent-warning/80 data-focus:text-spenny-accent-warning/80">
+						Edit
+					</button>
+				</MenuItem>
+				<MenuItem>
+					<button className="flex w-full items-center gap-2 rounded-lg p-1.5 transition duration-300 ease-in-out bg-spenny-accent-error/80 data-focus:bg-spenny-background data-focus:border-1 data-focus:border-solid data-focus:border-spenny-accent-error/80 data-focus:text-spenny-accent-error/80">
+						Delete
+					</button>
+				</MenuItem>
+			</MenuItems>
+		</Menu>
+	);
 };
 
 export default ActionMenu;
