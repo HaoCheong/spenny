2706c854d311da8ccd536b091c59ee8581393f37
hcheong <61342120+HaoCheong@users.noreply.github.com>
2025-07-07 15:03:47 +1000

    Updated index to use new favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
-// import "./App.css";
 import Dashboard from "./routes/Dashboard";
 import Logs from "./routes/Logs";
 
