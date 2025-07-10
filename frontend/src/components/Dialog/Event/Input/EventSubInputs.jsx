12b32b301b0c77cfd01f64ed73aad0a80a8267be
hao.cheong <61342120+HaoCheong@users.noreply.github.com>
2025-07-10 11:20:39 +1000

    Refactored out all the Event Inputs to separate collections

---
+++ frontend/src/components/Dialog/Event/Input/EventSubInputs.jsx
@@ -0,0 +1,27 @@
+import { Input } from "@headlessui/react";
+import clsx from "clsx";
+import FieldLabel from "../../../FieldLabel";
+
+const EventSubInputs = ({ formik }) => {
+	return (
+		<FieldLabel label="Amount to Deduct">
+			<Input
+				required
+				id="amount"
+				name="amount"
+				className={clsx(
+					"mt-2 w-full rounded-lg border-none bg-white/5 p-1.5 text-sm text-white",
+					"focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/30"
+				)}
+				onChange={(e) => {
+					formik.setFieldValue("properties", {
+						amount: parseInt(e.target.value),
+					});
+				}}
+				value={formik.values.properties.amount ?? 0}
+			/>
+		</FieldLabel>
+	);
+};
+
+export default EventSubInputs;
